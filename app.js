cb5ba1619091da36c10649dc9c823e703db8bdc1
mauricioferrer1 <mauricio@ferrer1.com>
2021-10-31 02:17:12 -0500

    enviando app.js

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const { get } = require("http");
 const app = express();
 
+app.use(express.static("public"))
+
 app.get("/", (req, res)=> {
     res.sendFile(__dirname + "/views/index.html")
 });
 
+app.get("/login", (req, res)=> {
+    res.sendFile(__dirname + "/views/login.html")
+});
 
-app.use(express.static("public"))
+app.get("/registro", (req, res)=> {
+    res.sendFile(__dirname + "/views/registro.html")
+});
+
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando puerto 3000');
