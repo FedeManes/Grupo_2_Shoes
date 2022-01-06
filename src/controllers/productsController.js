7209c66c8bfa0d96ab7d18002b45f9e7ace9b2f0
mauricioferrer1 <mauricio@ferrer1.com>
2022-01-06 00:36:37 -0500

    moviendo artefactos a directorios products y users

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,13 +8,13 @@ const controller = {
 
     listarProductos: (req,res) => {
       
-         res.render('products', {products})
+         res.render('products/products', {products})
       },
 
     detalleProducto: (req,res) => {
         let id = req.params.id
 		    let product = products.find (product => product.id == id)
-		    res.render('detail', {product})
+		    res.render('products/detail', {product})
       }
 
       
